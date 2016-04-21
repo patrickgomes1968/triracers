0125b79ee0ec5c7b235b7a33bc03da047c31b99a
Patrick Gomes <patrickgomes@hotmail.com>
2016-04-20 23:43:12 -0500

    attribute delegation tests all passed

--- app/models/racer_info.rb
+++ app/models/racer_info.rb
@@ -8,10 +8,21 @@ class RacerInfo
   field :racer_id, as: :_id
 	field :_id, default:->{ racer_id }
 
-	embedded_in :parent, polymorphic: true
+  embedded_in :parent, polymorphic: true
 
 	validates_presence_of :first_name, :last_name, :gender, :birth_year
 	validates :gender, inclusion: { in: %w(M F), message: "must be M or F" }
 	validates :birth_year, numericality: { less_than: Time.now.year, message: "Must be in  the past" }
 
+  ["city", "state"].each do |action|
+    define_method("#{action}") do
+      self.residence ? self.residence.send("#{action}") : nil
+    end
+    define_method("#{action}=") do |name|
+      object = self.residence ||= Address.new
+      object.send("#{action}=", name)
+      self.residence = object
+    end
+  end
+  
 end
