727c95bb66d4da2c7c46f3313056540da1bab109
Patrick Gomes <patrickgomes@hotmail.com>
2016-04-20 21:58:33 -0500

    updated racer to use delegation of methods

--- app/models/entrant.rb
+++ app/models/entrant.rb
@@ -15,7 +15,7 @@ class Entrant
 
   embeds_many :results, class_name: "LegResult", order: [:"event.o".asc], after_add: :update_total
   embeds_one :race, class_name: "RaceRef", autobuild: true
-  #embeds_one :racer, class_name: "RacerInfo", as: :parent, autobuild: true
+  embeds_one :racer, class_name: "RacerInfo", as: :parent, autobuild: true
 
   def update_total(result)
     self.secs = results.reduce(0) do |total, result|
